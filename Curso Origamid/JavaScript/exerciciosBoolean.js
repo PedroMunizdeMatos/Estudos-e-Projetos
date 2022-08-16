35457d1649ee753e530e37e780f8910b5961056c
Pedro Muniz <phmunizdematos@gmail.com>
2022-08-16 12:54:29 -0300

    update

---
+++ Curso Origamid/JavaScript/exerciciosBoolean.js
@@ -0,0 +1,58 @@
+// Verifique se a sua idade é maior do que a de algum parente
+var minhaIdade = 24
+var maeIdade = 57
+var paiIdade = 56
+
+if (minhaIdade > maeIdade) {
+  console.log('mano, como tu consegue ser mais velho que sua mãe ?')
+} else if (minhaIdade > paiIdade) {
+  console.log('mano, tu é mais velho q teu pai ??!')
+} else {
+  console.log('Você é o mais novo da sua família.')
+}
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - ' ' && 5 - 2
+console.log(expressao)
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+
+var nome = 'Andre'
+var idade = 28
+var possuiDoutorado = false
+var empregoFuturo
+var dinheiroNaConta = 0
+
+console.log(
+  !!nome,
+  !!idade,
+  !!possuiDoutorado,
+  !!empregoFuturo,
+  !!dinheiroNaConta
+)
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207
+var china = 1340
+
+if (brasil > china) {
+  console.log('brasil tem mais habitantes.')
+} else if (china > brasil) {
+  console.log('China tem mais habitantes.')
+} else brasil === china
+console.log('Ambos possuem a mesma quantidade de habitantes.')
+
+// O que irá aparecer no console?
+if ('Gato' === 'gato' && 5 > 2) {
+  console.log('Verdadeiro')
+} else {
+  console.log('Falso')
+}
+
+// O que irá aparecer no console?
+if ('Gato' === 'gato' || 5 > 2) {
+  console.log('Gato' && 'Cão')
+} else {
+  console.log('Falso')
+}
