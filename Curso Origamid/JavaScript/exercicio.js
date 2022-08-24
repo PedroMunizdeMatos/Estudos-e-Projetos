8b6a0c004f03bafb103b5a07f5a84447ba160c52
Pedro Muniz <phmunizdematos@gmail.com>
2022-08-24 09:24:46 -0300

    update js

---
+++ Curso Origamid/JavaScript/exercicio.js
@@ -0,0 +1,18 @@
+// Retorne o url da página atual utilizando o objeto window
+
+const href = window.location.href
+
+console.log(href)
+
+if (href === 'http://127.0.0.1:5500/JavaScript/index.html') {
+  console.log('É igual')
+}
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const selecionaA = document.querySelector('a')
+const selecionaClasseA = selecionaA.classList
+
+// Retorne a linguagem do navegador
+
+// Retorne a largura da janela
