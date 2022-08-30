978186eddd421db4e70628cf8edaca0452546280
Pedro Muniz <phmunizdematos@gmail.com>
2022-08-30 12:52:09 -0300

    att

--- Curso Origamid/Curso JavaScript/Estudos-js/exercicio.js
+++ Curso Origamid/Curso JavaScript/Estudos-js/exercicio.js
@@ -1,18 +1,30 @@
 // Retorne o url da página atual utilizando o objeto window
 
-const href = window.location.href
+// const href = window.location.href
 
-console.log(href)
+// console.log(href)
 
-if (href === 'http://127.0.0.1:5500/JavaScript/index.html') {
-  console.log('É igual')
-}
+// if (href === 'http://127.0.0.1:5500/JavaScript/index.html') {
+//   console.log('É igual')
+// }
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
 
-const selecionaA = document.querySelector('a')
-const selecionaClasseA = selecionaA.classList
+// const selecionaA = document.querySelector('a')
+// const selecionaClasseA = selecionaA.classList
 
 // Retorne a linguagem do navegador
 
 // Retorne a largura da janela
+
+// Retorne no console todas as imagens do site
+
+const allImg = document.querySelectorAll('img')
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
